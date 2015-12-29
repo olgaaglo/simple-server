3a61995f9dfd205b644be5f5217c972e8f804b0f
Olga Dourassof <Olga@Olgas-MacBook-Air.local>
2015-12-29 18:22:04 -0500

    secret phrase to people

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function (req, res) {
-  res.end('Here is your random number: ' + Math.random())
+
+
+  var parsedUrl = url.parse(req.url, true);
+  var query = parsedUrl.query;
+
+  var page = 'Here is your random number: ' + Math.random();
+
+  if (query.secret) {
+    if (query.secret === 'sauce') {
+      page = 'my secret area';
+    }
+  }
+
+
+
+  res.end(page)
 })
 
 server.listen(process.env.PORT || 3000)
