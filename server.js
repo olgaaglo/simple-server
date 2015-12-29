ec9cf5febf25a60a28e071f9a65a3093dcb13abe
Olga Dourassof <Olga@Olgas-MacBook-Air.local>
2015-12-29 17:46:15 -0500

    change to random number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  res.end('hello Olga')
+  res.end('Here is your random number: ' + Math.random())
 })
 
 server.listen(process.env.PORT || 3000)
